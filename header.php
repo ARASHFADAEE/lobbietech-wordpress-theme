2a5e86a50c1441e6005f7389fad620015c2047d6
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2023-10-07 05:06:06 -0700

    start wordpress theme develop

---
+++ header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="description" content="">
+  
+  <!-- Google Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,400i,500,700' rel='stylesheet'>
+
+  <!-- Css -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/font-icons.css" />
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/style.css" />
+
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri()?>/img/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri()?>/img/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri()?>/img/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri()?>/img/apple-touch-icon-114x114.png">
+<?php wp_head()?>
+</head>
+
+<body>
+
+  <!-- Preloader -->
+  <div class="loader-mask">
+    <div class="loader">
+      "Loading..."
+    </div>
+  </div>
+
+  <main class="main-wrapper">
+
+    <!-- Navigation -->
+    <header class="nav">
+      <div class="nav__holder nav--sticky">
+        <div class="container-fluid container-semi-fluid nav__container">
+          <div class="flex-parent">
+
+            <div class="nav__header">
+              <!-- Logo -->
+              <a href="index.html" class="logo-container flex-child">
+                <img class="logo" src="<?php echo get_template_directory_uri()?>/img/logo.png" srcset="<?php echo get_template_directory_uri()?>/img/logo.png 1x, img/logo@2x.png 2x" alt="logo">
+              </a>
+
+              <!-- Mobile toggle -->
+              <button type="button" class="nav__icon-toggle" id="nav__icon-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="nav__icon-toggle-bar"></span>
+                <span class="nav__icon-toggle-bar"></span>
+                <span class="nav__icon-toggle-bar"></span>
+              </button> 
+            </div>                      
+
+            <!-- Navbar -->
+            <nav id="navbar-collapse" class="nav__wrap collapse navbar-collapse">
+              <ul class="nav__menu">
+                <li>
+                  <a href="index.html">Home</a>
+                </li>
+                <li class="nav__dropdown">
+                  <a href="#">Pages</a>
+                  <i class="ui-arrow-down nav__dropdown-trigger"></i>
+                  <ul class="nav__dropdown-menu">
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="pricing.html">Pricing</a></li>
+                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                  </ul>
+                </li>
+                <li class="nav__dropdown">
+                  <a href="blog.html">Features</a>
+                  <i class="ui-arrow-down nav__dropdown-trigger"></i>
+                  <ul class="nav__dropdown-menu">
+                    <li><a href="icons.html">Icons</a></li>
+                    <li><a href="elements.html">Elements</a></li>
+                  </ul>
+                </li>
+                <li class="nav__dropdown active">
+                  <a href="blog.html">Blog</a>
+                  <i class="ui-arrow-down nav__dropdown-trigger"></i>
+                  <ul class="nav__dropdown-menu">
+                    <li><a href="blog.html">Blog Posts</a></li>
+                    <li><a href="single-post.html">Single Post</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Purchase</a>
+                </li>
+              </ul> <!-- end menu -->
+            </nav> <!-- end nav-wrap -->
+
+            <div class="nav__btn-holder nav--align-right">
+              <a href="#" class="btn nav__btn">
+                <span class="nav__btn-text">Call us for Free</span>
+                <span class="nav__btn-phone">63-995-3959</span>
+              </a>
+            </div>     
+        
+          </div> <!-- end flex-parent -->
+        </div> <!-- end container -->
+
+      </div>
+    </header> <!-- end navigation -->
---
+++ index.php
@@ -0,0 +1,327 @@
+<?php get_header()?>
+    <div class="content-wrapper oh">
+
+      <!-- Page Title -->
+      <section class="page-title text-center">
+        <div class="container">
+          <div class="page-title__holder">
+            <h1 class="page-title__title">Blog</h1>
+            <p class="page-title__subtitle">Recent news about marketing and SEO</p>
+          </div>
+        </div>
+      </section> <!-- end page title -->
+
+      <!-- Blog -->
+      <section class="section-wrap bottom-divider">
+        <div class="container">
+          <div class="row card-row">
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_1.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">27</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Effective Marketing for Start-Ups</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_2.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">23</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Offer an Outrageous or Daily Deal</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_3.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">21</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Investigate your Competition</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_4.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">18</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Using Technology to Maximize Efficiency</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_5.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">15</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Breaking One Task Down Into Bite-Size Pieces</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_6.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">12</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Get Organized - Technology is your Friend</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_7.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">8</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Be Able to Meet Short Notice Deadlines</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_8.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">6</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">9 Essential Parts of a Business Plan</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div class="col-lg-4">
+              <article class="entry card box-shadow hover-up">
+                <div class="entry__img-holder card__img-holder">
+                  <a href="single-post.html">
+                    <img src="img/blog/post_9.jpg" class="entry__img" alt="">
+                  </a>
+                  <div class="entry__date">
+                    <span class="entry__date-day">4</span>
+                    <span class="entry__date-month">aug</span>
+                  </div>
+                  <div class="entry__body card__body">                    
+                    <h4 class="entry__title">
+                      <a href="single-post.html">Keeping Your Ideas Organized</a>
+                    </h4>
+                    <ul class="entry__meta">
+                      <li class="entry__meta-category">
+                        <i class="ui-category"></i>
+                        <a href="#">Marketing</a>
+                      </li>
+                      <li class="entry__meta-comments">
+                        <i class="ui-comments"></i>
+                        <a href="#">3 Comments</a>
+                      </li>
+                    </ul>                     
+                    <div class="entry__excerpt">
+                      <p>Build a brand for your company using a specific color scheme and a professional logo designed with your business in mind.</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+          </div>
+
+          <!-- Pagination -->
+          <nav class="pagination">
+            <span class="pagination__page pagination__page--current">1</span>
+            <a href="#" class="pagination__page">2</a>
+            <a href="#" class="pagination__page">3</a>
+            <a href="#" class="pagination__page">4</a>
+            <a href="#" class="pagination__page pagination__icon pagination__page--next"><i class="ui-arrow-right"></i></a>
+          </nav>
+
+        </div>
+      </section> <!-- end blog -->
+
+
+      <!-- Footer -->
+<?php get_footer( )?>
+
+  
+</body>
+</html>
