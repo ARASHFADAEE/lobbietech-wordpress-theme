5542f4de44a436bf322f2ecf080137b4cb4e5b2a
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2023-10-07 11:31:17 -0700

    custom logo support in theme

--- functions.php
+++ functions.php
@@ -45,5 +45,17 @@ function custom_excerpt_more($more) {
 }
 add_filter('excerpt_more', 'custom_excerpt_more');
 
-
+//custom logo in theme
+function themename_custom_logo_setup() {
+	$defaults = array(
+		'height'               => 39,
+		'width'                => 123,
+		'flex-height'          => true,
+		'flex-width'           => true,
+		'header-text'          => array( 'site-title', 'site-description' ),
+		'unlink-homepage-logo' => true, 
+	);
+	add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
 
